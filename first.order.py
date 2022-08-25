014d1528a080cf7eef9e22add68b5f4dcbe447cd
kishorpunem <kishornani01@gmail.com>
2022-08-25 23:25:41 +0530

    This is my first automation code

---
+++ first.order.py
@@ -0,0 +1,16 @@
+import time
+
+import pytest
+from selenium.webdriver import Edge
+from selenium.webdriver.common.by import By
+
+def test_Logintotheapplication():
+    path = "C:\\Users\\Kishor\\Downloads\\edgedriver_win64 (1)\\msedgedriver.exe"
+    driver = Edge(executable_path=path)
+    driver.get("http://test.trukker.ae/")
+    driver.maximize_window()
+    time.sleep(5)
+    driver.find_element(By.ID, "btnLoginLink").click()
+    driver.find_element(By.ID, "user_id").send_keys("srivani.koneti@trukker.com")
+    driver.find_element(By.ID, "password").send_keys("123")
+    driver.find_element(By.ID, "btnLogin").click()
